eb8da1e927f3ce52d06acb3a9051b589d328306c
Thyrum <adj00@protonmail.com>
2025-10-20 14:24:57 +0200

    Add a first workable version

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,41 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import "./style.css";
+import songsJson from "./data/songs.json";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+type Song = {
+  number: string;
+  title: string;
+  subtitle?: string;
+  artist: string;
+  key: string;
+  links: {
+    lyrics?: string;
+  } & Record<string, string>;
+};
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const songs = songsJson as Song[];
+
+const list = document.getElementById("songList") as HTMLUListElement;
+for (const song of songs) {
+  const listItem = document.createElement("li");
+  listItem.className = "song-item";
+  listItem.style = `--number: "${song.number}";`;
+  listItem.innerHTML = `
+		<div class="song-header">
+		  <span class="song-title">
+				${song.title}
+				${song.subtitle ? `<span class="song-subtitle"> (${song.subtitle})</span>` : ""}
+			</span>
+			<div class="song-key-div"><span class="song-key">Key: ${song.key}</span></div>
+		</div>
+		<div class="song-artist">by ${song.artist}</div>
+		<div class="song-links">
+			${Object.entries(song.links)
+        .map(
+          ([label, url]) =>
+            `<a href="${url}" target="_blank" rel="noopener noreferrer">${label}</a>`,
+        )
+        .join(" | ")}
+		</div>
+	`;
+  list.appendChild(listItem);
+}
